6db3360f93e63ad0e449c58d24ba35fd1126eef6
abm-noman <noman15-2839@diu.edu.bd>
2023-08-30 02:33:16 +0600

    Front End part added

---
+++ resources/views/home/new_arrival.blade.php
@@ -0,0 +1,23 @@
+<section class="arrival_section">
+         <div class="container">
+            <div class="box">
+               <div class="arrival_bg_box">
+                  <img src="images/arrival-bg.png" alt="">
+               </div>
+               <div class="row">
+                  <div class="col-md-6 ml-auto">
+                     <div class="heading_container remove_line_bt">
+                        <h2>
+                           #NewArrivals
+                        </h2>
+                     </div>
+                     <p style="margin-top: 20px;margin-bottom: 30px;">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore amet eveniet obcaecati ab deserunt in architecto hic, reprehenderit possimus? Laboriosam, culpa non! Rem quam asperiores, necessitatibus enim ex et aspernatur!</p>
+                     <a href="">
+                     Shop Now
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
